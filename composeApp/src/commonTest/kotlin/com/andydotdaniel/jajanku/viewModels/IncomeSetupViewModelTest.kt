265d9253343658d8ee929925415b6a297142d3aa
Andrew Daniel <andydotdaniel@gmail.com>
2026-01-26 00:58:38 +0700

    Fix unit tests with scheduler

--- composeApp/src/commonTest/kotlin/com/andydotdaniel/jajanku/viewModels/IncomeSetupViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/com/andydotdaniel/jajanku/viewModels/IncomeSetupViewModelTest.kt
@@ -4,28 +4,42 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.andydotdaniel.jajanku.mocks.IncomeRepositoryMock
 import com.andydotdaniel.jajanku.ui.pages.setup.income.IncomeSetupViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class IncomeSetupViewModelTest {
 
-    private val mockIncomeRepository = IncomeRepositoryMock()
+    private val testDispatcher = StandardTestDispatcher()
+    private lateinit var mockIncomeRepository: IncomeRepositoryMock
 
     private lateinit var viewModel: IncomeSetupViewModel
 
     @BeforeTest
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+
+        mockIncomeRepository = IncomeRepositoryMock()
         mockIncomeRepository.savedIncome = 8500000.0
         viewModel = IncomeSetupViewModel(mockIncomeRepository)
     }
 
+    @AfterTest
+    fun tearDown() {
+        // 3. After each test, clean up the main dispatcher to avoid affecting other tests
+        Dispatchers.resetMain()
+    }
 
     @Test
-    fun testGetIncome() = runTest {
+    fun testGetIncome() = runTest(testDispatcher) {
         advanceUntilIdle()
         assertThat(viewModel.uiState.value.income).isEqualTo("8500000.0")
     }
@@ -39,7 +53,7 @@ class IncomeSetupViewModelTest {
     }
 
     @Test
-    fun testSaveIncome() = runTest {
+    fun testSaveIncome() = runTest(testDispatcher) {
         val newIncomeValue = "4829000"
 
         viewModel.updateIncome(newIncomeValue)
