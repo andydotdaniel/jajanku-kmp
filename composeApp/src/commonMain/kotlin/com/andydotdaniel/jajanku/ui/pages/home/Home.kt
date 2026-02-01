490680461e15e0e1fdaeb45d9b61eaf56fd7fabe
Andrew Daniel <andydotdaniel@gmail.com>
2026-02-02 00:51:31 +0700

    Fix color issue for drawable icons

--- composeApp/src/commonMain/kotlin/com/andydotdaniel/jajanku/ui/pages/home/Home.kt
+++ composeApp/src/commonMain/kotlin/com/andydotdaniel/jajanku/ui/pages/home/Home.kt
@@ -1,18 +1,24 @@
 package com.andydotdaniel.jajanku.ui.pages.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -23,9 +29,13 @@ import com.andydotdaniel.jajanku.ui.components.ExpenseListItem
 import com.andydotdaniel.jajanku.ui.components.SegmentedPillControl
 import com.andydotdaniel.jajanku.ui.platformSafeContentPadding
 import com.andydotdaniel.jajanku.utils.AppColor
+import jajanku.composeapp.generated.resources.Res
+import jajanku.composeapp.generated.resources.icon_settings_24px
+import jajanku.composeapp.generated.resources.settings_24px
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
-import kotlin.String
 
 class Home: Screen {
 
@@ -53,57 +63,81 @@ class Home: Screen {
 @Preview
 @Composable
 fun PreviewHomeScreen() {
-    val data = listOf<ExpenseItem>(
-        ExpenseItem(
-            id = 1,
-            icon = "\uD83C\uDF54",
-            amount = "Rp 24103",
-            category = "Food",
-            time = "12:00",
-        ),
-        ExpenseItem(
-            id = 2,
-            icon = "\uD83C\uDF79",
-            amount = "Rp 12103",
-            category = "Drink",
-            time = "11:00",
-        ),
-        ExpenseItem(
-            id = 3,
-            icon = "\uD83C\uDF79",
-            amount = "Rp 73103",
-            description = "Some long text about the item description that's long",
-            category = "Drink",
-            time = "09:00",
-        )
+    Scaffold (
+        topBar = {
+            Row {
+                IconButton(
+                    onClick = { /* Handle click */ },
+                    modifier = Modifier
+                        .size(36.dp) // Set a fixed size
+                        .clip(CircleShape) // Clip the button to a circle
+                        .background(AppColor.BackgroundGray) // Set the background color
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.settings_24px),
+                        contentDescription = stringResource(Res.string.icon_settings_24px), // Essential for accessibility
+                        tint = AppColor.White // Set the icon's color
+                    )
+                }
+            }
+        },
+        containerColor = AppColor.Black,
+        content = {
+            val data = listOf<ExpenseItem>(
+                ExpenseItem(
+                    id = 1,
+                    icon = "\uD83C\uDF54",
+                    amount = "Rp 24103",
+                    category = "Food",
+                    time = "12:00",
+                ),
+                ExpenseItem(
+                    id = 2,
+                    icon = "\uD83C\uDF79",
+                    amount = "Rp 12103",
+                    category = "Drink",
+                    time = "11:00",
+                ),
+                ExpenseItem(
+                    id = 3,
+                    icon = "\uD83C\uDF79",
+                    amount = "Rp 73103",
+                    description = "Some long text about the item description that's long",
+                    category = "Drink",
+                    time = "09:00",
+                )
+            )
+
+            LazyColumn(modifier = Modifier.platformSafeContentPadding()) {
+                item {
+                    Text("Remaining Budget", color = AppColor.White, fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
+                    Text("234000", color = AppColor.White, fontSize = 48.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 4.dp))
+
+                    val options = listOf("Monthly", "Weekly", "Daily")
+                    SegmentedPillControl(modifier = Modifier.padding(top = 16.dp), options = options, 1) {
+
+                    }
+                }
+
+                item {
+                    Spacer(modifier = Modifier.height(48.dp))
+                    Text("Today's Expenses", color = AppColor.White, fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
+                    Spacer(modifier = Modifier.height(24.dp))
+                }
+
+                items(count = data.size) { index ->
+                    val item = data[index]
+
+                    ExpenseListItem(item, false) {}
+                    if (index < data.size - 1) {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        HorizontalDivider(thickness = 1.dp, color = Color(0xFF333333))
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+                }
+            }
+        }
     )
 
-    LazyColumn(modifier = Modifier.platformSafeContentPadding()) {
-        item {
-            Text("Remaining Budget", color = AppColor.White, fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
-            Text("234000", color = AppColor.White, fontSize = 48.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 4.dp))
 
-            val options = listOf("Monthly", "Weekly", "Daily")
-            SegmentedPillControl(modifier = Modifier.padding(top = 16.dp), options = options, 1) {
-
-            }
-        }
-
-        item {
-            Spacer(modifier = Modifier.height(48.dp))
-            Text("Today's Expenses", color = AppColor.White, fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
-            Spacer(modifier = Modifier.height(24.dp))
-        }
-
-        items(count = data.size) { index ->
-            val item = data[index]
-
-            ExpenseListItem(item, false) {}
-            if (index < data.size - 1) {
-                Spacer(modifier = Modifier.height(16.dp))
-                HorizontalDivider(thickness = 1.dp, color = Color(0xFF333333))
-                Spacer(modifier = Modifier.height(16.dp))
-            }
-        }
-    }
 }
